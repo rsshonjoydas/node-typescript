ded5ce521a28a5439f7c223b2214d3262c661c92
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-07 13:36:03 +0600

    dynamic routes

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,50 @@
 "use strict";
-console.log('rs shonjoy');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// ? external imports
+const express_1 = __importDefault(require("express"));
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// ? internal imports
+const PORT = process.env.PORT || 5000;
+const init = () => {
+    let app = express_1.default();
+    // TODO: dynamic routing setup
+    let routes = [
+        {
+            http: 'get',
+            path: '/',
+            handler: (req, res) => {
+                res.send(`thanks ${req.method}`);
+            },
+        },
+        {
+            http: 'get',
+            path: '/rs',
+            handler: (req, res) => {
+                res.send(`thanks for rs ${req.method}`);
+            },
+        },
+        {
+            http: 'get',
+            path: '/shonjoy',
+            handler: (req, res) => {
+                res.send(`thanks for shonjoy ${req.method}`);
+            },
+        },
+    ];
+    routes.forEach((route) => {
+        app[route.http](route.path, route.handler);
+    });
+    app.use('/', (req, res) => {
+        res.send('RS Shonjoy' + new Date());
+    });
+    return app;
+};
+const app = init();
+app.listen(PORT, () => {
+    console.log(`app listing to port ${PORT}`);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,60 @@
-console.log('rs shonjoy');
+// ? external imports
+import express, { Application, Request, Response } from 'express';
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// ? internal imports
+
+const PORT = process.env.PORT || 5000;
+
+export type Handler = (req: Request, res: Response) => void;
+
+interface IRoute {
+  http: string;
+  path: string;
+  handler: Handler;
+}
+
+const init = (): Application => {
+  let app = express();
+
+  // TODO: dynamic routing setup
+  let routes: IRoute[] = [
+    {
+      http: 'get',
+      path: '/',
+      handler: (req: Request, res: Response) => {
+        res.send(`thanks ${req.method}`);
+      },
+    },
+    {
+      http: 'get',
+      path: '/rs',
+      handler: (req: Request, res: Response) => {
+        res.send(`thanks for rs ${req.method}`);
+      },
+    },
+    {
+      http: 'get',
+      path: '/shonjoy',
+      handler: (req: Request, res: Response) => {
+        res.send(`thanks for shonjoy ${req.method}`);
+      },
+    },
+  ];
+
+  routes.forEach((route) => {
+    (app as any)[route.http](route.path, route.handler);
+  });
+
+  app.use('/', (req, res) => {
+    res.send('RS Shonjoy' + new Date());
+  });
+
+  return app;
+};
+
+const app = init();
+app.listen(PORT, () => {
+  console.log(`app listing to port ${PORT}`);
+});
